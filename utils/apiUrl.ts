652353270c296774aa9b03dbe37d9561cb18850f
Nghiem Thanh Pham <nghiempt.dev@gmail.com>
2023-12-17 17:02:40 +0700

    fix code port

--- utils/apiUrl.ts
+++ utils/apiUrl.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8000/api/v1';
+export const BASE_URL = 'http://dsppsurvey.pfamilies.me:82/api/v1';
 
 export const ApiUrl = {
   // ============== APP ==============
